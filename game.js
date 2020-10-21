08f2139aa6a13d956c18889530c6333d47383b4e
ghoroubbostaji <63856672+ghoroubbostaji@users.noreply.github.com>
2020-10-21 16:04:38 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -19,10 +19,10 @@ function rollingDice() {
         var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
         document.querySelector(".img1").setAttribute("src",
-            + randomNumber1 + ".PNG");
+            + randomNumber1 + ".png");
 
         document.querySelector(".img2").setAttribute("src",
-            + randomNumber2 + ".PNG");
+            + randomNumber2 + ".png");
 
         if (randomNumber1 === randomNumber2) {
             document.querySelector("h2").innerHTML = "Rolling again!";
